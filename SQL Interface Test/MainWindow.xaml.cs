d0e76364ed22d2916548778a3c532a643d6de935
frogzlegz <abrain5423@gmail.com>
2021-10-13 14:08:26 +0100

    first commit

---
+++ SQL Interface Test/MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+namespace SQL_Interface_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string tableString = "obj2";
+        int nextID;
+        public static string consoleMessage;
+        public static string consolePrev;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _classes.clsDB.CurrentConnection.OpenConnection();
+            Refresh();
+
+            InitTable();
+            //b();
+
+            //_classes.clsDB.CurrentConnection.CloseConnection();
+        }
+
+        public void ChangeTbl(string tblStr)
+        {
+            tableString = tblStr;
+        }
+
+        public void ChangeDB(string dbStr)
+        {
+            _classes.clsDB.nm_DB_Current = dbStr;
+        }
+
+
+        public void b()
+        {
+            DataTable table = null;
+
+            try
+            {
+                table = _classes.clsDB.Get_DataTable(tableString);
+            }
+            catch (Exception e)
+            {
+                _classes.clsDB.Execute_SQL("CREATE TABLE " + tableString + " (ID int, str varchar(100));");
+                table = _classes.clsDB.Get_DataTable(tableString);
+            }
+
+            Grunk grunk = new Grunk();
+            grunk.grunkint = 4;
+            grunk.grunkstr = "three";
+
+            try
+            {
+                _classes.clsDB.Get_DataTable(tableString);
+            }
+            catch
+            {
+                _classes.clsDB.Execute_SQL("CREATE TABLE " + tableString + " (ID int, str varchar(100));");
+            }
+            finally
+            {
+                _classes.clsDB.Execute_SQL("INSERT INTO " + tableString + " VALUES (" + grunk.grunkint + ", " + "'" + grunk.grunkstr + "'" + ");");
+            }
+        }
+
+        public class Grunk
+        {
+            public int grunkint;
+            public string grunkstr;
+        }
+
+        private void InitTable()
+        {
+            DataTable table = null;
+
+            try
+            {
+                table = _classes.clsDB.Get_DataTable(tableString);
+
+                if (table == null)
+                    throw new Exception();
+            }
+            catch (Exception e)
+            {
+                _classes.clsDB.Execute_SQL("CREATE TABLE " + tableString + " (ID int, str varchar(100))");
+                table = _classes.clsDB.Get_DataTable("SELECT * FROM " + tableString);
+            }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _classes.clsDB.Execute_SQL("INSERT INTO " + tableString + " VALUES (" + nextID + ", '" + Name_Box.Text + "');");
+            }
+            catch (Exception f)
+            {
+                UpdateMessage(f.Message);
+            }
+
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            Name_Box.Text = null;
+
+            try
+            {
+                if (_classes.clsDB.Get_DataTable(tableString) == null)
+                    throw new Exception();
+            }
+            catch (Exception e)
+            {
+                _classes.clsDB.Execute_SQL("CREATE TABLE " + tableString + " (ID int, str varchar(100));");
+            }
+
+            nextID = _classes.clsDB.Get_Last_ID(tableString) + 1;
+
+            ID_Label.Content = nextID.ToString();
+
+            DB_Selection.Text = _classes.clsDB.nm_DB_Current;
+
+            Table_Selection.Text = tableString;
+
+            Console_Output.Content = consoleMessage == consolePrev ? "" : consoleMessage;
+        }
+
+        /// TODO:
+        /// Handle error when invalid DB name used - Exception in Get_DB_Connection();
+        /// System tables - retrieve tables
+        /// AutoIncrement ID
+
+        private void DB_Change_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeDB(DB_Selection.Text);
+            Refresh();
+        }
+
+        private void Table_Change_Click(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"^[\p{L}_][\p{L}\p{N}@$#_]{0,127}$");
+
+            if (regex.Match(Table_Selection.Text) == null)
+            {
+                try
+                {
+                    ChangeTbl(Table_Selection.Text);
+              
+                    //throw new Exception("ERROR: Table could not be changed!");
+                }
+                catch (Exception f)
+                {
+                    UpdateMessage(f.Message);
+                }
+            }
+            else
+            {
+                UpdateMessage("Invalid Table Name!");
+            }
+
+            Refresh();
+        }
+
+        public static void UpdateMessage(string message)
+        {
+            consolePrev = consoleMessage;
+            consoleMessage = message;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ChangeServer(Server_Selection.SelectedItem.ToString());
+            Refresh();
+        }
+        public void ChangeServer(string server)
+        {
+            const string prefix = "System.Windows.Controls.ComboBoxItem: ";
+
+            switch (server)
+            {
+                case prefix + "MySQL":
+                    {
+                        _classes.clsDB.CurrentConnection.connectionString = _classes.clsDB.PathDB_MySQL.connectionString;
+                        break;
+                    }
+                case prefix + "Local":
+                    {
+                        _classes.clsDB.CurrentConnection.connectionString = _classes.clsDB.PathDB_Local.connectionString;
+                        break;
+                    }
+                case prefix + "VS":
+                    {
+                        _classes.clsDB.CurrentConnection.connectionString = _classes.clsDB.PathDB_VS.connectionString;
+                        break;
+                    }
+                default:
+                    {
+                        _classes.clsDB.CurrentConnection.connectionString = null;
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ SQL Interface Test/_classes/clsDB.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+namespace SQL_Interface_Test._classes
+
+{
+    public static class clsDB
+    {
+        public class DatabaseConnection
+        {
+            public ConnectionType connectionType;
+            public string connectionString;
+            public DbConnection connection;
+
+            public DatabaseConnection(ConnectionType connectionType, string connectionString)
+            {
+                this.connectionType = connectionType;
+                this.connectionString = connectionString;
+            }
+
+            public DbConnection Connect()
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                    connection = null;
+                }
+
+                switch (connectionType)
+                {
+                    default:
+                    case ConnectionType.MySql:
+                        {
+                            connection = new MySqlConnection(connectionString);
+                            break;
+                        }
+                    case ConnectionType.Local:
+                    case ConnectionType.VS:
+                        {
+                            connection = new SqlConnection(connectionString);
+                            break;
+                        }
+                }
+
+                return connection;
+            }
+
+            public DbConnection OpenConnection()
+            {
+                try
+                {
+                    DbConnection connection = CurrentConnection.Connect();
+                    connection.Open();
+                    return connection;
+                }
+                catch (SqlException e)
+                {
+                    MainWindow.UpdateMessage(e.Message);
+                }
+
+                return null;
+            }
+
+            public DbCommand CreateCommand(string sql)
+            {
+                switch (this.connectionType)
+                {
+                    default:
+                    case ConnectionType.MySql:
+                        return new MySqlCommand(sql, (MySqlConnection)connection);
+                    case ConnectionType.Local:
+                    case ConnectionType.VS:
+                        return new SqlCommand(sql, (SqlConnection)connection);
+                }
+            }
+        }
+
+        public enum ConnectionType {MySql, Local, VS}
+
+        // DB names
+        public static string nm_DB_Local = "db_Local";
+        public static string nm_DB_VS = "db_local";
+        public static string nm_DB_Current = nm_DB_VS;
+
+
+        // Extra step for VS DB. Not required for Local
+
+        // public static string dir_DB_VS = "C:\\Users\\boggl_000\\source\\repos\\SQL Interface Test\\SQL Interface Test\\_data\\" + nm_DB_Current + ".mdf";
+        public static string dir_DB_VS = "H:\\My Documents\\Development\\Year_2\\VS2019\\SQL Interface Test\\SQL Interface Test\\_data\\" + nm_DB_Current + ".mdf"; 
+
+        // Path Definitions
+        public static DatabaseConnection PathDB_VS = new DatabaseConnection(ConnectionType.Local, "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"" + dir_DB_VS + "\";Integrated Security=True");
+        public static DatabaseConnection PathDB_Local = new DatabaseConnection(ConnectionType.Local, "Data Source=(localdb)\\ProjectsV13;Initial Catalog=" + nm_DB_Current + ";Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public static DatabaseConnection PathDB_MySQL = new DatabaseConnection (ConnectionType.MySql, "server=127.0.0.1;uid=root;pwd=Gibson111!;persistsecurityinfo=True;database=msql_DB_1");
+
+        // Current path/DB
+        public static string Path_Current;
+        public static string DB_Current;
+        public static DatabaseConnection CurrentConnection = PathDB_VS;
+
+        public static ConnectionType Connection_Type = ConnectionType.MySql;
+
+        public static DataTable Get_DataTable(string tableName)
+        {                        
+            if (CurrentConnection.connection == null || CurrentConnection.connection.State != ConnectionState.Open)
+                return null;
+
+            DbCommand command = CurrentConnection.CreateCommand("SELECT * FROM " + tableName);
+
+            DbDataReader reader1 = command.ExecuteReader();
+
+            DataTable table = new DataTable();
+
+            if (reader1.Read())
+            {
+                table.Load(reader1);
+            }
+
+            reader1.Close();
+
+            return table;            
+        }
+
+        public static int Get_Last_ID(string tableName)
+        {
+            int lastID = 0;
+
+            DbCommand command = CurrentConnection.CreateCommand("SELECT MAX(ID) FROM " + tableName);
+
+            DbDataReader reader1;
+            try
+            {
+                reader1 = command.ExecuteReader();
+
+                if (reader1.Read())
+                {
+                    try
+                    {
+                        lastID = ((IDataRecord)reader1).GetInt32(0);
+                    }
+                    catch (System.Data.SqlTypes.SqlNullValueException e)
+                    {
+                        MainWindow.UpdateMessage(e.Message);
+                    }
+                }
+
+                reader1.Close();
+            }
+            catch (SqlException e)
+            {
+                MainWindow.UpdateMessage(e.Message);
+            }
+
+            return lastID;
+        }
+        private static void ReadSingleRow(IDataRecord record)
+        {
+            Console.WriteLine(String.Format("{0}, {1}", record[0], record[1]));
+        }
+
+        public static void Execute_SQL(string SQL_Text)
+        {
+            CurrentConnection.OpenConnection();
+            DbCommand command = CurrentConnection.CreateCommand(SQL_Text);
+
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                MainWindow.UpdateMessage(e.Message);
+            }
+        }
+
+        public static DataSet Retrieve_Data(string SQL_Text)
+        {
+            DbCommand command = CurrentConnection.CreateCommand(SQL_Text);
+
+            DbDataReader reader1;
+            reader1 = command.ExecuteReader();
+
+            reader1.Close();
+
+            return null;
+        }
+    }
+}
